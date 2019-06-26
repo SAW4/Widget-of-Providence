63314bf0e1f244c01ba15b5a1a17b1f0ea306098
Sato Sawa <sawayeung@gmail.com>
2019-06-27 01:09:17 +0800

    presist Uri Permission - reboot

--- app/src/main/java/me/webhop/sawanet/widgetofprovidence/ImagePicker.java
+++ app/src/main/java/me/webhop/sawanet/widgetofprovidence/ImagePicker.java
@@ -4,8 +4,11 @@ import android.app.Activity;
 import android.appwidget.AppWidgetManager;
 import android.content.ComponentName;
 import android.content.ContentResolver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.net.Uri;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
@@ -16,6 +19,12 @@ import static android.content.ContentValues.TAG;
 
 public class ImagePicker extends AppCompatActivity {
 
+    // appWidgetId init: first launch didnt have any image path bind to widget
+    private int appWidgetId = AppWidgetManager.INVALID_APPWIDGET_ID;
+
+    // user defined final request code: identify result intent type
+    private int REQUEST_CODE = 8993;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,6 +34,7 @@ public class ImagePicker extends AppCompatActivity {
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT){
             img_picker = new Intent(Intent.ACTION_OPEN_DOCUMENT);
             img_picker.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+            img_picker.addCategory(Intent.CATEGORY_OPENABLE);
             img_picker.addFlags(Intent.FLAG_GRANT_PERSISTABLE_URI_PERMISSION);
         }else{
             img_picker = new Intent(Intent.ACTION_GET_CONTENT);
@@ -32,32 +42,58 @@ public class ImagePicker extends AppCompatActivity {
         img_picker.putExtra(Intent.EXTRA_LOCAL_ONLY, true);
         img_picker.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
         img_picker.setType("image/*");
-        startActivityForResult(Intent.createChooser(img_picker, "Select Picture"), 0);
+
+
+        // Request code 8993 -> image picker result intent
+        startActivityForResult(Intent.createChooser(img_picker, "Select Picture"), REQUEST_CODE);
         setContentView(R.layout.activity_image_picker);
+
+        Bundle extras = getIntent().getExtras();
+        assert extras != null;
+        this.appWidgetId = extras.getInt(AppWidgetManager.EXTRA_APPWIDGET_ID, AppWidgetManager.INVALID_APPWIDGET_ID);
+        Log.v(TAG, "ImagePiceker -> this.appWidgetId: " + appWidgetId);
     }
 
     @Override
     public void onActivityResult(final int requestCode, final int resultCode, final Intent data) {
-        switch (resultCode) {
-            case RESULT_CANCELED:finishAndRemoveTask(); return;
+
+        // if it is not called by image picker, end
+        if (requestCode != REQUEST_CODE || resultCode == RESULT_CANCELED) {
+            finish();
+            return;
         }
+
+        // if everything OKAY, update image
         try {
             // get the selected result (img uri)
             Uri uri = data.getData();
             Intent intent = getIntent();
             assert uri != null;
             // get widget id and image uri
-            int appWidgetId = intent.getIntExtra("appWidgetId", -1);
+            int appWidgetId = intent.getIntExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, AppWidgetManager.INVALID_APPWIDGET_ID);
             // create intent to pass data back to widget (update)
             Intent widgetIntent = new Intent(this, Widget.class);
             widgetIntent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
             widgetIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
             widgetIntent.putExtra("uri", uri.toString());
+            widgetIntent.setFlags(data.getFlags());
+
+            final int takeFlags = data.getFlags()
+                    & (Intent.FLAG_GRANT_READ_URI_PERMISSION
+                    | Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+            ContentResolver resolver = getContentResolver();
+            resolver.takePersistableUriPermission(uri, takeFlags);
+            Log.v(TAG, "Persist-Permission => " + uri);
+            Log.v(TAG, "widgetIntent uri Extra => " + uri );
+            // update shared preference
+            SharedPreferences pref = getSharedPreferences(getPackageName(), Context.MODE_PRIVATE);
+            pref.edit().putString(Integer.toString(appWidgetId), uri.toString()).apply();
+
             // Send bundle back to widget by using broadcast, widget's onReceive() can get it
             sendBroadcast(widgetIntent);
         } catch (NullPointerException ex) {
             ex.printStackTrace();
         }
-        finishAndRemoveTask();
+        finish();
     }
 }
--- app/src/main/java/me/webhop/sawanet/widgetofprovidence/Widget.java
+++ app/src/main/java/me/webhop/sawanet/widgetofprovidence/Widget.java
@@ -1,22 +1,20 @@
 package me.webhop.sawanet.widgetofprovidence;
 
-import android.annotation.SuppressLint;
+import android.app.DownloadManager;
 import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.database.Cursor;
 import android.graphics.Bitmap;
-import android.graphics.Canvas;
 import android.graphics.Matrix;
-import android.graphics.Paint;
-import android.graphics.Point;
-import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.util.Log;
-import android.view.Display;
-import android.view.WindowManager;
-import android.widget.ImageView;
 import android.widget.RemoteViews;
 
 import com.bumptech.glide.Glide;
@@ -24,6 +22,9 @@ import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.bumptech.glide.request.RequestOptions;
 import com.bumptech.glide.request.target.AppWidgetTarget;
 import com.bumptech.glide.request.transition.Transition;
+
+import java.net.URI;
+import java.util.List;
 
 import static android.content.ContentValues.TAG;
 
@@ -35,12 +36,12 @@ import static android.content.ContentValues.TAG;
  */
 public class Widget extends AppWidgetProvider {
     private AppWidgetTarget appWidgetTarget;
+    private SharedPreferences pref;
     /**
      * @param bitmap the Bitmap to be scaled
      * @param threshold the maxium dimension (either width or height) of the scaled bitmap
      * @param isNecessaryToKeepOrig is it necessary to keep the original bitmap? If not recycle the original bitmap to prevent memory leak.
      * */
-
     public static Bitmap getScaledDownBitmap(Bitmap bitmap, int threshold, boolean isNecessaryToKeepOrig){
         int width = bitmap.getWidth();
         int height = bitmap.getHeight();
@@ -99,16 +100,35 @@ public class Widget extends AppWidgetProvider {
     }
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+
+        pref = context.getSharedPreferences(context.getPackageName(), Context.MODE_PRIVATE);
         for (int appWidgetId : appWidgetIds) {
+            String pref_str = pref.getString(Integer.toString(appWidgetId), null);
             Intent intent = new Intent(context, ImagePicker.class);
             intent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
-            intent.putExtra("appWidgetId", appWidgetId); // pass widget id (home screen can have many)
-            Log.d(TAG, "onUpdate called, widget id: " + appWidgetId + " Bind to onclick event.");
+            intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId); // pass widget id (home screen can have many)
+            Log.v(TAG, "onUpdate called, widget id: " + appWidgetId + " Bind to onclick event.");
+            Log.v(TAG, "onUpdate called, pref_str: " + pref_str );
+
             // Pending intent is a intent that will not execute at once, but interact with onclick to the widget
             PendingIntent pendingIntent = PendingIntent.getActivity(context, appWidgetId, intent, 0);
             RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget);
             views.setOnClickPendingIntent(R.id.widget_layout, pendingIntent);
+
+            if (pref_str != null){
+                // create intent to pass data back to widget (update)
+                Intent  widgetIntent = new Intent(context, Widget.class);
+                widgetIntent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+                widgetIntent.addFlags(Intent.FLAG_GRANT_PERSISTABLE_URI_PERMISSION);
+                widgetIntent.putExtra(Intent.EXTRA_LOCAL_ONLY, true);
+                widgetIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                widgetIntent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
+                widgetIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
+                widgetIntent.putExtra("uri", pref_str);
+                context.sendBroadcast(widgetIntent);
+            }
             appWidgetManager.updateAppWidget(appWidgetId, views);
+
         }
     }
 
@@ -121,25 +141,31 @@ public class Widget extends AppWidgetProvider {
                 int widgetId = intent.getExtras().getInt(AppWidgetManager.EXTRA_APPWIDGET_ID);
                 // If record not find, add this new path
                 String receiveData = intent.getExtras().getString("uri");
-                Log.d(TAG, "onReceive called, unpacking data");
+                int appWidgetId = intent.getExtras().getInt(AppWidgetManager.EXTRA_APPWIDGET_ID);
+                pref = context.getSharedPreferences(context.getPackageName(), Context.MODE_PRIVATE);
+
+                Log.v(TAG, "onReceive called: Image is picked");
+                Log.v(TAG, "onReceive -> receiveData := " + receiveData);
+                Log.v(TAG, "onReceive -> appWidgetId := " + appWidgetId);
+
                 if (receiveData != null) {
                     Uri imgUri = Uri.parse(receiveData);
                     // Create a new remote view, change the image of it then update to the widget
                     RemoteViews control = new RemoteViews(context.getPackageName(), R.layout.widget);
-                    Log.d(TAG, "[Receive] widget id : " + String.valueOf(widgetId) + " Uri= " + imgUri);
-
                     appWidgetTarget = new AppWidgetTarget(context, R.id.widget_image, control, widgetId) {
                         @Override
                         public void onResourceReady(Bitmap resource, Transition<? super Bitmap> transition) {
                             // compress or resize here
                             int threshold = 1024;
-                            resource = getScaledDownBitmap(resource, threshold, true);
+                            resource = getScaledDownBitmap(resource, threshold, false);
                             super.onResourceReady(resource, transition);
                         }
                     };
+                    Log.v(TAG, "imgUri => " + imgUri);
                     RequestOptions requestOptions = new RequestOptions();
                     requestOptions.diskCacheStrategy(DiskCacheStrategy.NONE);
                     requestOptions.skipMemoryCache(true);
+                    requestOptions.fitCenter();
                     Glide.with(context).asBitmap().load(imgUri).apply(requestOptions).into(appWidgetTarget);
                     AppWidgetManager.getInstance(context).updateAppWidget(widgetId, control);
                 }
